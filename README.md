4850376e2f1ac1c8e3c4d9ebe335936eecc74801
acenturyandabit <acenturyandabit@gmail.com>
2024-03-07 22:03:15 +1100

    figure out what I actually need and implement it (no more userscript though :( )

--- README.md
+++ README.md
@@ -8,17 +8,32 @@ Requirements: node/npm on your system
 
 1. Clone this folder to somewhere out of the way.
 2. Run `npm install .`
-3. Run the server: `./server.js`
+3. Add the `bashrc_supplement` to your .bashrc; restart shells or source .bashrc
+4. Run the server: `./server.js`
    - You may want to use a https server (even a self-signed one) as this will allow userscripts from https sites to send requests to your server. To do this:
        1. `openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"`
        2. Run instead with `HTTPS=true ./server.js`
-4. Open the webapp according to the console output.
-5. Set up the bash task pusher.
-    1. Run `./bash-tracker.sh`
-6. Set up userscripts.
-    1. Download the fantastic Tampermonkey extension from the chrome web store: <https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo>
-    2. Create a userscript for the target website.
-    3. Copy the contents of `userscript.js` into your userscript body.
-    4. Update the task creation and mark-as-done
+       3. Add `STACK_HTTPS=1` to your bashrc.
+5. Open the webapp according to the console output.
+6. For new shells, run `stack-register-thread TASKNAME` to register a shell to a task. (You might want to alias this, its pretty long)
+7. Start a long running task on your shell that you've registered. Stack will be alerted to its presence.
+8. When the task is done, Stack will let you know.
+9. Ack that the task is done using `stack-ack` in the same shell as the process, then keep doing your work.
 
 ## How it works
+
+There are three actors in our model: You, the Computer, and Stack. You have a number of threads of work. Your job is to do your most urgent thread; but sometimes you ask the Computer to do your thread, e.g. compile a piece of code. When this happens, you will go work on another, less urgent thread. When the Computer has finished on your most urgent thread, the Stack will notify you that you are able to resume your most urgent thread.
+
+(Note that the Computer can represent a build job, a cloud runner, or even another person - anything that is blocking your highest priority thread.)
+
+A thread can be in one of four states: You're working on it, You could be working on it, the Computer is working on it, or Dead (done or cancelled).
+
+## What Stack actually keeps track of
+
+Stack can't actually keep track of threads-of-work; instead it can only keep track of PIDs on the system (of bash shells), or browser windows.
+
+You should register bash shells by typing `stack-register-thread TASKNAME` to associate a bash shell to a thread. Subsequently, Stack, via `server.js`, will check whether or not the shell has any child processes. If any shell associated with the thread has a child process, Stack will assume that the Computer is working on the thread. If all shells associated with the the thread dies, Stack will assume the thread is dead.
+
+The `server.js` will give the frontend a list of tasks, their state, and any metadata for that state (e.g. process names).
+
+The frontend keeps a record of thread priority, and will display in large font the highest priority thread that is you're-working-on-it. This is stored in the frontend localstorage, because I can't be bothered to make a way for the server to store it right now.
--- server.js
+++ server.js
@@ -2,27 +2,61 @@
 import express from 'express'
 import fs from 'fs'
 import https from 'https'
+import { execSync } from "child_process";
 import { WebSocketServer } from 'ws';
 import cors from 'cors';
 
 const PORT = 6234;
 
+/*
+type task {
+    thread_id: string,
+    state: "YOU_DOING" | "YOU_COULD_BE_DOING" | "DEAD" | "COMPUTER_DOING"
+};
+*/
+
+
 const main = () => {
     const live_connections = [];
-    const app = build_app(live_connections);
+    const thread_state = {
+        threads: {},
+        pid_thread_mapping: {}
+    };
+
+    const app = build_app(thread_state, live_connections);
+
+    watch_pids(thread_state, live_connections);
+
     const server = start_http_or_https_server(app, process.env["HTTPS"] == "true");
     start_websocket_server(server, live_connections);
 }
 
 
-const build_app = (live_connections) => {
+const build_app = (thread_state, live_connections) => {
     const app = express();
     app.use(cors());
     app.use(express.static("frontend"));
     app.post("/status", (req, res) => {
-        if (req.query.shell_id) {
+        if (req.query.type == "associate-pid") {
+            thread_state.pid_thread_mapping[req.query.pid] = req.query.thread_id;
+            thread_state.threads[req.query.thread_id] = {
+                thread_id: req.query.thread_id,
+                state: "YOU_DOING"
+            }
             live_connections.forEach(conn => {
-                conn.send(JSON.stringify(req.query))
+                conn.send(JSON.stringify(thread_state.threads[req.query.thread_id]));
+            });
+            res.status(200).end();
+        } else if (req.query.type == "post-update") {
+            let thread_id = req.query.thread_id;
+            if (!thread_id) {
+                thread_id = thread_state.pid_thread_mapping[req.query.pid];
+            }
+            if (thread_state.threads[thread_id].state != req.query.state) {
+                thread_state.threads[thread_id].state = req.query.state;
+            }
+            live_connections.forEach(conn => {
+                conn.send(JSON.stringify(thread_state.threads[thread_id]));
             });
             res.status(200).end();
         } else {
@@ -62,5 +96,56 @@ const start_websocket_server = (http_server, live_connections) => {
     return live_connections;
 }
 
+const watch_pids = (thread_state, live_connections) => {
+    setInterval(() => {
+        let pids = Object.keys(thread_state.pid_thread_mapping);
+        let thread_pids = {};
+        // check for alive threads
+        for (let thread_id in thread_state.threads) {
+            thread_state.threads[thread_id].pids = [];
+        }
+        // check if PIDs are still alive
+        pids.forEach(pid => {
+            try {
+                execSync(`ps -p ${pid}`);
+                thread_state.threads[thread_state.pid_thread_mapping[pid]].pids.push(pid);
+                // console.log(`thread ${pid} still alive`)
+            } catch (e) {
+                delete thread_state.pid_thread_mapping[pid];
+                // console.log(`thread ${pid} dead`)
+            }
+        });
+        // check dead PIDs
+        for (let thread_id in thread_state.threads) {
+            if (thread_state.threads[thread_id].pids.length == 0) {
+                // RIP
+                thread_state.threads[thread_id].state = "DEAD";
+            }
+        }
+
+        let still_alive_pids = Object.keys(thread_state.pid_thread_mapping);
+        still_alive_pids.forEach(pid => {
+            const thread_id = thread_state.pid_thread_mapping[pid];
+            const current_state = thread_state.threads[thread_id].state;
+            const future_state = { ...thread_state.threads[thread_id] };
+            try {
+                const child_pid = execSync(`pgrep -P ${pid}`).toString().split("\n")[0];
+                // console.log(`thread ${pid} has child ${child_pid}`)
+                future_state.state = "COMPUTER_DOING";
+                future_state.command = execSync(`ps -o args= -p ${child_pid}`).toString()
+            } catch (e) {
+                // console.log(e)
+                // console.log(`thread ${pid} has no child`)
+                if (future_state.state == "COMPUTER_DOING") {
+                    future_state.state = "YOU_COULD_BE_DOING";
+                }
+            }
+            thread_state.threads[thread_id] = future_state;
+            live_connections.forEach(conn => {
+                conn.send(JSON.stringify(thread_state.threads[thread_id]));
+            });
+        });
+    }, 1000);
+}
 
 main();
