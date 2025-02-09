de6cd7e746ba5af8408e8708994ad6f282105402
acenturyandabit <acenturyandabit@gmail.com>
2025-02-10 07:48:35 +1100

    TTL support

--- HTTPTaskPoller.ts
+++ HTTPTaskPoller.ts
@@ -3,26 +3,44 @@ import { Express } from "express";
 
 type TaskCallback = (tasks: Record<string, Task>) => void;
 
+type HTTPPollerTask = {
+  name: string,
+  last_seen: number,
+  completed_at: number | null,
+  ttl: number
+}
+
 export default class HTTPTaskPoller {
   private interval: number;
   private callback: TaskCallback;
   private poller?: NodeJS.Timeout;
-  private tasks: Record<string, Task> = {};
+  private innerTasks: Record<string, HTTPPollerTask> = {};
 
   constructor(interval: number, callback: TaskCallback, app: Express) {
     this.interval = interval;
     this.callback = callback;
     app.post("/status", (req, res) => {
-      const pid = req.query.thread_id as string;
-      this.tasks[pid] = {
-        pid,
-        cwd: "-",
-        command: req.query.thread_id as string,
-        started_at: Date.now(), // use as last seen
-        completed_at: null
+      const name = req.query.thread_id as string;
+      this.innerTasks[name] = {
+        name,
+        last_seen: Date.now(), // use as last seen
+        completed_at: null,
+        ttl: parseInt(req.query.ttl as string || "4000")
       };
-      this.callback(this.tasks);
+      this.callback(this.tasks());
     });
+  }
+
+  tasks() {
+    return Object.fromEntries(Object.entries(this.innerTasks).map(([_, task])=>{
+      return [_, {
+        pid: task.name,
+        cwd: "-",
+        command: task.name,
+        started_at: task.last_seen,
+        completed_at: task.completed_at
+      }]
+    })) as Record<string, Task>
   }
 
   start() {
@@ -39,22 +57,21 @@ export default class HTTPTaskPoller {
   }
 
   acknowledge(task_id: string) {
-    delete this.tasks[task_id];
-    this.callback(this.tasks);
+    delete this.innerTasks[task_id];
+    this.callback(this.tasks());
   }
 
   private pollTasks() {
-    const oldTaskString = JSON.stringify(this.tasks);
-    this.tasks = Object.fromEntries(Object.entries(this.tasks).map(([_, task]) => {
-      if (task.started_at < Date.now() - 4000 && task.completed_at === null) {
+    const oldTaskString = JSON.stringify(this.innerTasks);
+    this.innerTasks = Object.fromEntries(Object.entries(this.innerTasks).map(([_, task]) => {
+      if (task.last_seen < Date.now() - task.ttl && task.completed_at === null) {
         task.completed_at = Date.now();
-        console.log("completed!")
       }
       return [_, {...task}];
     }));
-    if (oldTaskString != JSON.stringify(this.tasks)) {
+    if (oldTaskString != JSON.stringify(this.innerTasks)) {
       console.log("emitted")
-      this.callback(this.tasks);
+      this.callback(this.tasks());
     }
   }
 }
